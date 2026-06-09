7847a485b95dfd7fa1842dffc0e383bccb51c693
ToPlay <85893696+ToPlay-byte@users.noreply.github.com>
2026-06-09 17:44:18 +0300

    Remove incorrect standalone integration app config

--- trustpilot_integrations/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class TrustpilotIntegrationsConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "trustpilot_integrations"
-    verbose_name = "Trustpilot Integrations"
