69897267b6d8e96c12cae377a1ad07d81b828331
ToPlay <85893696+ToPlay-byte@users.noreply.github.com>
2026-06-09 17:29:09 +0300

    Add Django app config

---
+++ trustpilot_integrations/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class TrustpilotIntegrationsConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "trustpilot_integrations"
+    verbose_name = "Trustpilot Integrations"
